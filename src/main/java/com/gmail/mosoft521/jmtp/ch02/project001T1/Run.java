8b3bea2db857e1f4cd57b6bf49e291753243a181
Alvin Zhang <mosoft521@gmail.com>
2015-08-01 23:01:07 +0800

    第二章 临时提交

---
+++ src/main/java/com/gmail/mosoft521/jmtp/ch02/project001T1/Run.java
@@ -0,0 +1,15 @@
+package com.gmail.mosoft521.jmtp.ch02.project001T1;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+        HasSelfPrivateNum numRef = new HasSelfPrivateNum();
+
+        ThreadA athread = new ThreadA(numRef);
+        athread.start();
+
+        ThreadB bthread = new ThreadB(numRef);
+        bthread.start();
+    }
+}
