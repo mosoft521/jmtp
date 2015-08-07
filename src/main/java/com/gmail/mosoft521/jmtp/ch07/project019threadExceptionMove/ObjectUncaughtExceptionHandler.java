38b4723b32c23b7be546ef1896580ad47073f27d
Alvin Zhang <mosoft521@gmail.com>
2015-08-07 23:41:55 +0800

    第七章 over

--- src/main/java/com/gmail/mosoft521/jmtp/ch07/project019threadExceptionMove/ObjectUncaughtExceptionHandler.java
+++ src/main/java/com/gmail/mosoft521/jmtp/ch07/project019threadExceptionMove/ObjectUncaughtExceptionHandler.java
@@ -4,8 +4,8 @@ import java.lang.Thread.UncaughtExceptionHandler;
 
 public class ObjectUncaughtExceptionHandler implements UncaughtExceptionHandler {
 
-	public void uncaughtException(Thread t, Throwable e) {
-		System.out.println("对象的异常处理");
-		e.printStackTrace();
-	}
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("对象的异常处理");
+        e.printStackTrace();
+    }
 }
